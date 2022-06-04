0e07722bb596dcb8366908442557845a50d62276
Toshihiko Asahi <lovingcat222@gmail.com>
2022-06-04 19:52:39 +0900

    dir関数

--- hellomodule.py
+++ hellomodule.py
@@ -3,6 +3,13 @@ import point
 Point = point.Point #自分定義の名前にしたい場合　本当はモジュール名.関数やクラス名でアクセス
 
 print(point.__name__)
+print(dir(point))
+#組み込み関数等はbuiltinsに入っている
+import builtins
+print(dir(builtins))
+#sysには動作環境に関する変数の情報が入っている
+import sys
+print(dir(sys))
 
 p1:Point = Point(3,4)
 p2:Point = Point(10,3)
